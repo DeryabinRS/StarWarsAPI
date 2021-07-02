5a531fd27db3023b7863e89e6686ea9a69000e4e
Deryabin Roman <deryabinrs@gmail.com>
2021-07-02 22:39:15 +0700

    favorite

--- src/pages/FavoriteHeroes.js
+++ src/pages/FavoriteHeroes.js
@@ -13,17 +13,30 @@ export const FavoriteHeroes = () => {
 		localStorage.setItem("chars", JSON.stringify(charsLS));
   	}, [charsLS]);
 
-	const getFavoriteHeroes = () => {
-
+	const delFavoriteHeroes = (charData) => {
+		if(charData.choosed){
+			const newCharLS = charsLS.filter(item => item.id !== charData.id);
+			setCharsLS(newCharLS);
+		}
 	}
 
-	return (
-		<Container className="mt-4">
-			<div className={styles.chars_block}>
-				{charsLS.map((item, i) => {
-					return <Character key={i} data={item} del={true}/>
-				})}
-			</div>
-		</Container>
-	)
+	if(charsLS.length > 0){
+		return (
+			<Container className="mt-4">
+				<div className={styles.chars_block}>
+					{charsLS.map((item, i) => {
+						return <Character key={i} data={item} del={true} onChooseChar={delFavoriteHeroes}/>
+					})}
+				</div>
+			</Container>
+		)
+	}else{
+		return (
+			<Container className="mt-4">
+				<div style={{color:'white'}}>
+					Empty favorite lits
+				</div>
+			</Container>
+		)
+	}
 }
